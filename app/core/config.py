89126a5fd148c776c3e3650ba0b722944f74501a
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-06-21 20:54:28 +0545

    feat : updated the config file to include env variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,25 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
+
+    DATABASE_URL: str
+
+    DEBUG: bool = False
+
+    SECRET_KEY: str
+
+    ALGORITHM: str = "HS256"
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
     model_config = SettingsConfigDict(
-        env_file="./.env",
+        env_file=".env",
         case_sensitive=False,
         extra="ignore"
     )
 
-    database_url: str
-    debug: bool = True
 
-settings=Settings()
+settings = Settings()
