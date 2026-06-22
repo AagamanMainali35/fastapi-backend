cfcbeedefab8455a2becbb31eb947e9592afa82d
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-06-22 12:09:16 +0545

    updated the router to use class based views

--- app/api/auth/router.py
+++ app/api/auth/router.py
@@ -1,14 +1,23 @@
 # Auth API router
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi_utils.cbv import cbv
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.api.auth.schemas import LoginRequest, RegisterRequest
+from app.core.db import get_db
+from app.domains.auth.services import Authservice
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post("/login")
-def login():
-    pass
+@cbv(router)
+class UserView:
+    session: AsyncSession = Depends(get_db)
 
-
-@router.post("/register")
-def register():
-    pass
+    @router.post("/register")
+    async def register(
+        self,
+        user: RegisterRequest,
+    ):
+        result = Authservice.register_user(self.session, user.email, user.username, user.password)
+        return await result
