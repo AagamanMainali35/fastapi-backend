e6037a4c3a21d28cf10fdf971249438ffd02af84
Aagaman Mainali <aagamanmainali35@gmail.com>
2026-06-22 11:19:33 +0545

    feat: created the service file

---
+++ app/api/auth/services.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.domains.auth.models import User
+from app.domains.auth.query import create_user, get_user_by_email
+
+
+def hash_password(password: str):
+    return password
+
+
+async def register_user(session: AsyncSession, email: str, username: str, password: str):
+
+    existing = await get_user_by_email(session, email)
+
+    if existing:
+        raise Exception("Email already exists")
+
+    user = User(email=email, user_name=username, hashed_password=hash_password(password))
+
+    return await create_user(session, user)
